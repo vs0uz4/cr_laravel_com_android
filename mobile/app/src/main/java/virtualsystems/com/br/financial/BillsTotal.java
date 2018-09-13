7e9785f655d1089b04adae56303c2fef9de248f5
Vitor S Rodrigues <vitor.rodrigues@gmail.com>
2018-09-13 00:21:13 -0300

    Created Bills Total Feature

--- mobile/app/src/main/java/virtualsystems/com/br/financial/BillsTotal.java
+++ mobile/app/src/main/java/virtualsystems/com/br/financial/BillsTotal.java
@@ -6,13 +6,33 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.text.NumberFormat;
+
+import cz.msebera.android.httpclient.HttpResponse;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.methods.HttpGet;
+import cz.msebera.android.httpclient.impl.client.HttpClientBuilder;
+import cz.msebera.android.httpclient.util.EntityUtils;
 
 public class BillsTotal extends Fragment {
+    private HttpClient httpClient = HttpClientBuilder.create().build();
+    private Double billTotals = 0.0;
+    private TextView txtBillsTotal;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle save){
-        return inflater.inflate(R.layout.fragment_bills_total, container, false);
+        View view = inflater.inflate(R.layout.fragment_bills_total, container, false);
+
+        txtBillsTotal = (TextView) view.findViewById(R.id.txt_bills_total);
+
+        return view;
     }
 
     @Override
@@ -20,5 +40,34 @@ public class BillsTotal extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         getActivity().setTitle("Totals of Bills");
+
+        try {
+            findBills();
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void findBills() throws IOException, JSONException {
+        HttpGet clientGet = new HttpGet("http://192.168.254.8/api/bill_pays");
+
+        clientGet.addHeader("Content-Type", "application/json");
+        clientGet.addHeader("Accept", "application/json");
+        clientGet.addHeader("Authorization", "Bearer " + UserSession.getInstance(getContext()).getUserToken());
+        HttpResponse response = httpClient.execute(clientGet);
+
+        String json = EntityUtils.toString(response.getEntity());
+        JSONObject result = new JSONObject(json);
+
+        for (int i = 0; i < result.getJSONArray("data").length(); i++) {
+            JSONObject data = result.getJSONArray("data").getJSONObject(i);
+
+            billTotals = billTotals + data.getDouble("value");
+
+            if (txtBillsTotal != null) {
+                NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
+                txtBillsTotal.setText(numberFormat.format(billTotals).toString());
+            }
+        }
     }
 }
